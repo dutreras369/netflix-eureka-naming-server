33bde6bac74ac973df09f14691e0f6b592cd06f6
dutreras <dutreras369@gmail.com>
2022-07-27 12:01:33 -0400

    first commit

---
+++ netflix-eureka-naming-server/src/test/java/cl/paseodelasdelicias/microservices/netflixeureka/NetflixEurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.paseodelasdelicias.microservices.netflixeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixEurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
